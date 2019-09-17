3a02551a7ea84407dd0b6b3511cf9d2addb027b4
icydwei <robpaik@gmail.com>
2019-09-16 22:14:03 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(records) {
+  let winrecord = records.find( function(s) { return s.result === 'W'})
+  if (Boolean (winrecord)) {
+    return winrecord.year
+  }
+}
